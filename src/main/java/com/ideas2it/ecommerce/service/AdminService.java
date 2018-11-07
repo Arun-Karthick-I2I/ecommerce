2f69c2831671d6575647493a99cd8384c34d3ed9
“Pavithra-Sivasubramanian” <pavithras@ideas2it.com>
2018-11-07 12:16:13 +0530

    ecommerce:Added features in AdminController

--- src/main/java/com/ideas2it/ecommerce/service/AdminService.java
+++ src/main/java/com/ideas2it/ecommerce/service/AdminService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.ideas2it.ecommerce.exception.EcommerceException;
 import com.ideas2it.ecommerce.model.Order;
+import com.ideas2it.ecommerce.model.Seller;
+import com.ideas2it.ecommerce.model.Customer;
 
 public interface AdminService {
     
@@ -25,4 +27,84 @@ public interface AdminService {
      * @return
      */
     Order searchByOrderId(Integer id) throws EcommerceException;
+    
+    /**
+     * <p>
+     * 
+     * </p>
+     * 
+     * @param status
+     * @return
+     */
+    List<Customer> getCustomers(Boolean status) throws EcommerceException;
+    
+    /**
+     * <p>
+     * 
+     * </p>
+     * 
+     * @param id
+     * @param status
+     * @return
+     */
+    Customer searchByCustomerId(Integer id, Boolean status) 
+        throws EcommerceException;
+    
+    /**
+     * <p>
+     * 
+     * </p>
+     * 
+     * @param name
+     * @param status
+     * @return
+     */
+    List<Customer> searchByCustomerName(String name, Boolean status) 
+            throws EcommerceException;
+    
+    /**
+     * <p>
+     * 
+     * </p>
+     * 
+     * @param customer
+     * @return
+     */
+    Boolean deleteCustomer(Customer customer) throws EcommerceException;
+    
+    /**
+     * 
+     * @return
+     */
+    List<Seller> getSellers() throws EcommerceException;
+    
+    /**
+     * <p>
+     * 
+     * </p>
+     * 
+     * @param id
+     * @return
+     */
+    Seller searchBySellerId(Integer id) throws EcommerceException;
+    
+    /**
+     * <p>
+     * 
+     * </p>
+     * 
+     * @param name
+     * @return
+     */
+    List<Seller> searchBySellerName(String name) throws EcommerceException;
+    
+    /**
+     * <p>
+     * 
+     * </p>
+     * 
+     * @param seller
+     * @return
+     */
+    Boolean deleteSeller(Seller seller) throws EcommerceException;
 }
